0a88225cc8ac138b55e79bab2af21b8537758f3d
Raju Talukder <63764709+Mostafaarju@users.noreply.github.com>
2021-10-24 20:58:08 +0200

    Add Game over UI

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ canvas.height = innerHeight;
 const scoreEl = document.querySelector('#scoreEl');
 const startGameBtn = document.querySelector('#startGameBtn');
 const modalEl = document.querySelector('#modalEl');
+const bigScoreEl = document.querySelector('#bigScoreEl');
 
 class Player {
   constructor(x, y, radius, color) {
@@ -161,6 +162,8 @@ function animate() {
     //   end game
     if (dist - enemy.radius - player.radius < 1) {
       cancelAnimationFrame(animationId);
+      modalEl.style.display = 'flex';
+      bigScoreEl.innerHTML = score;
     }
 
     projectiles.forEach((projectile, projectileIndex) => {
